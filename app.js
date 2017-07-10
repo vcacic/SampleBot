c7503b715b0522fe8ca34c96d93b4e5cb182121e
VedranCacic <vedran.cacic@clover-studio.com>
2017-07-10 15:50:54 +0200

    first commit1

---
+++ app.js
@@ -0,0 +1,22 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(8080, function () {
+   console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: "f5a70d70-0268-444e-b206-918e49573783",
+    appPassword: "cVu4nU7RLtrpsYNLgRfqbQi"
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("Clover echoes: %s", session.message.text);
+});
