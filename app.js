196bcd5ad7f147cbdffbae1730006aabbb99ab64
VedranCacic <vedran.cacic@clover-studio.com>
2017-07-12 14:04:30 +0200

    only restify

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
+
 server.listen(8080, function () {
    console.log('%s listening to %s', server.name, server.url);
 });
@@ -13,8 +14,18 @@ var connector = new builder.ChatConnector({
     appPassword: "cVu4nU7RLtrpsYNLgRfqbQi"
 });
 
+
+function respond(req, res, next) {
+  console.log(req);
+  res.send('hello ' + req.params);
+  next();
+}
+
 // Listen for messages from users
-server.post('/api/messages', connector.listen());
+// server.post('/api/messages', connector.listen());
+server.get('/messages', respond);
+server.post('/messages', respond);
+
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
