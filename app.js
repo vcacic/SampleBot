c3ee391b01421562b41fb75c923fe403bf37c3dc
VedranCacic <vedran.cacic@clover-studio.com>
2017-07-13 16:16:08 +0200

    clearEchoBot

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var rp = require('request-promise');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -14,18 +15,8 @@ var connector = new builder.ChatConnector({
     appPassword: "cVu4nU7RLtrpsYNLgRfqbQi"
 });
 
-
-function respond(req, res, next) {
-  console.log(req);
-  res.send('hello ' + req.params);
-  next();
-}
-
 // Listen for messages from users
-// server.post('/api/messages', connector.listen());
-server.get('/messages', respond);
-server.post('/messages', respond);
-
+server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
